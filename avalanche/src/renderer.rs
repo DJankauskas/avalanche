bd8d85acb0b4a6b9e318f906c7f03e79eb713879
Darius Jankauskas <d5926j@gmail.com>
2020-10-12 01:28:19 -0400

    Convert vnode functionality to use index-based tree

--- avalanche/src/renderer.rs
+++ avalanche/src/renderer.rs
@@ -2,26 +2,36 @@ use std::any::Any;
 
 use crate::{Component, View};
 use crate::vdom::{VNode};
-use crate::{InternalContext, shared::Shared};
+use crate::{InternalContext};
 
 ///An opaque handle whose underlying type is determined by the current `Renderer`.
 pub type NativeHandle = Box<dyn Any>;
 
 pub trait Renderer {
-    fn create_component(&mut self, vnode: &VNode) -> Option<NativeHandle>;
+    fn create_component(
+        &mut self, 
+        native_type: &NativeType, 
+        component: &View,
+        children: &mut dyn Iterator<Item=Child>
+    ) -> Option<NativeHandle>;
     
+    //TODO: make native_handle take &mut?
     fn update_component(
         &mut self, 
         native_type: &NativeType, 
         native_handle: &mut NativeHandle, 
-        old_comp: &View,
-        new_comp: &View,
-        children: &[Shared<VNode>]
+        component: &View,
+        children: &mut dyn Iterator<Item=Child>
     );
 
     fn remove_component(&mut self, vnode: &mut VNode);
 
     fn schedule_on_ui_thread(&mut self, f: Box<dyn FnOnce()>);
+}
+#[non_exhaustive]
+pub struct Child<'a> {
+    pub component: &'a View,
+    pub native_handle: Option<&'a NativeHandle> 
 }
 
 pub struct NativeType {
