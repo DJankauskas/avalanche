1635eef28cf89657d934a1b8369d5198ef051e38
Darius Jankauskas <d5926j@gmail.com>
2020-12-28 23:11:32 -0500

    Enclose macro

--- avalanche/src/lib.rs
+++ avalanche/src/lib.rs
@@ -1,8 +1,8 @@
-///A trait providing platform-specific rendering code.
+/// A trait providing platform-specific rendering code.
 pub mod renderer;
-///A reference-counted interior-mutable type designed to reduce runtime borrow rule violations.
+/// A reference-counted interior-mutable type designed to reduce runtime borrow rule violations.
 pub mod shared;
-///An in-memory representation of the current component tree.
+/// An in-memory representation of the current component tree.
 pub mod vdom;
 
 pub mod tree;
@@ -20,12 +20,34 @@ macro_rules! reactive_assert {
     ( $( $($dept:ident),+ => $rec:ident );* ) => {};
 }
 
-///For internal use only, does not obey semver and is unsupported
-///A hack used to work around a seeming syn limitation
-///syn will interpret some macro calls as items rather than expressions
-///syn expects an item to be replaced by another, so when parsing Component! type macros,
-///`#[reactive]` will replace that call with an expression within this macro
-///to syn, this is replacing one macro item with another
+/// Takes a list of identifiers terminated by a semicolon and expression. Each identifier is
+/// cloned, and made available to the expression. The macro evaluates to that expression.
+/// This is useful for passing things like state and setters to multiple component props.
+/// # Example
+/// ```rust
+/// # use avalanche::enclose;
+/// let message = "Enclose me!".to_owned();
+/// let closure1 = enclose!(message; move || println!("{}", message));
+/// let closure2 = enclose!(message; move || eprintln!("{}", message));
+/// ```
+// Note: code from stdweb
+// TODO: appropriately license project and/or code snippet as MIT or Apache license
+#[macro_export]
+macro_rules! enclose {
+    ( $( $x:ident ),*; $y:expr ) => {
+        {
+            $(let $x = $x.clone();)*
+            $y
+        }
+    };
+}
+
+/// For internal use only, does not obey semver and is unsupported
+/// A hack used to work around a seeming syn limitation
+/// syn will interpret some macro calls as items rather than expressions
+/// syn expects an item to be replaced by another, so when parsing Component! type macros,
+/// `#[reactive]` will replace that call with an expression within this macro
+/// to syn, this is replacing one macro item with another
 #[doc(hidden)]
 #[macro_export]
 macro_rules! __internal_identity {
@@ -34,8 +56,8 @@ macro_rules! __internal_identity {
     };
 }
 
-///A reference-counted type that holds an instance of a component.
-///Component functions must return a view.
+/// A reference-counted type that holds an instance of a component.
+/// Component functions must return a view.
 #[derive(Clone)]
 pub struct View {
     rc: Rc<dyn DynComponent>,
@@ -61,8 +83,8 @@ impl<T: DynComponent> From<T> for View {
     }
 }
 
-///The trait representing a component. Except for renderer libraries,
-///users should not implement this trait manually but instead use the `component` attribute.
+/// The trait representing a component. Except for renderer libraries,
+/// users should not implement this trait manually but instead use the `component` attribute.
 pub trait Component: 'static {
     type Builder;
 
@@ -88,7 +110,7 @@ pub trait Component: 'static {
 }
 
 impl Component for () {
-    //TODO: make ! when never stabilizes
+    // TODO: make ! when never stabilizes
     type Builder = ();
 
     fn render(&self, _: InternalContext) -> View {
@@ -99,8 +121,8 @@ impl Component for () {
     }
 }
 
-///An internal trait implemented for all `Component`s. This should not be
-///implemented manually.
+/// An internal trait implemented for all `Component`s. This should not be
+/// implemented manually.
 pub trait DynComponent: Downcast + 'static {
     #[doc(hidden)]
     fn init_state(&self) -> Box<dyn Any>;
@@ -148,8 +170,8 @@ impl<T: Component> DynComponent for T {
 }
 
 #[doc(hidden)]
-///Provided as an argument to the [`Component::render`] method
-///to provide hooks access to component state and other data.
+/// Provided as an argument to the [`Component::render`] method
+/// to provide hooks access to component state and other data.
 pub struct InternalContext<'a> {
     pub state: &'a mut Box<dyn Any>,
     pub component_pos: ComponentPos,
@@ -157,8 +179,8 @@ pub struct InternalContext<'a> {
 
 #[doc(hidden)]
 #[derive(Clone)]
-///Internal data structure that stores what tree a component
-///belongs to, and its position within it
+/// Internal data structure that stores what tree a component
+/// belongs to, and its position within it
 pub struct ComponentPos {
     ///Shared value ONLY for passing to UseState
     ///within the render function this value is mutably borrowed,
@@ -168,7 +190,7 @@ pub struct ComponentPos {
     pub vdom: Shared<VDom>,
 }
 
-///A hook that allows a component to keep persistent state across renders.
+/// A hook that allows a component to keep persistent state across renders.
 pub struct UseState<T: 'static> {
     state: Option<T>,
     updated: bool,
@@ -184,8 +206,8 @@ impl<T> Default for UseState<T> {
 }
 
 impl<T> UseState<T> {
-    ///This function is called by the framework. Users gain access to a reference to the
-    ///state type and a setter when they call it.
+    /// This function is called by the framework. Users gain access to a reference to the
+    /// state type and a setter when they call it.
     pub fn hook<'a>(
         &'a mut self,
         component_pos: ComponentPos,
@@ -223,7 +245,7 @@ impl<T: 'static> Clone for UseStateSetter<T> {
     fn clone(&self) -> Self {
         Self {
             component_pos: self.component_pos.clone(),
-            get_mut: self.get_mut
+            get_mut: self.get_mut,
         }
     }
 }
@@ -239,9 +261,9 @@ impl<T: 'static> UseStateSetter<T> {
         }
     }
 
-    ///Takes a function that modifies its attached state and triggers a rerender of its component.
-    ///The update is not performed immediately; its effect will only be accessible
-    ///on its component's rerender.
+    /// Takes a function that modifies its attached state and triggers a rerender of its component.
+    /// The update is not performed immediately; its effect will only be accessible
+    /// on its component's rerender.
     pub fn call<F: FnOnce(&mut T)>(&self, f: F) {
         let get_mut = self.get_mut;
         let vdom_clone = self.component_pos.vdom.clone();
@@ -258,11 +280,11 @@ impl<T: 'static> UseStateSetter<T> {
                 vdom_clone.exec_mut(|vdom| {
                     vnode_copy.get_mut(&mut vdom.tree).dirty = true;
                     update_vnode(
-                        None, 
-                        vnode_copy, 
-                        &mut vdom.tree, 
-                        &mut vdom.renderer, 
-                        vdom_clone_2
+                        None,
+                        vnode_copy,
+                        &mut vdom.tree,
+                        &mut vdom.renderer,
+                        vdom_clone_2,
                     );
                 })
             }));
