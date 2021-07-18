43e59892c7249dc2ef1127346314e1eb4104ba35
Darius Jankauskas <d5926j@gmail.com>
2021-07-18 01:47:32 -0400

    Add panic hook.

--- avalanche-web/examples/counter/src/lib.rs
+++ avalanche-web/examples/counter/src/lib.rs
@@ -28,5 +28,10 @@ fn Counter() -> View {
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
 pub fn main_js() {
+    // This provides better error messages in debug mode.
+    // It's disabled in release mode so it doesn't bloat up the file size.
+    #[cfg(debug_assertions)]
+    console_error_panic_hook::set_once();
+    
     avalanche_web::mount_to_body::<Counter>();
 }
